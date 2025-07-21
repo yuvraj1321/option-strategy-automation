9be17065c5867da87050dc5cbc51a5b6bd91d8a6
yuvraj1321 <89725931+yuvraj1321@users.noreply.github.com>
2025-07-21 11:31:40 +0530

    Update Option_Intraday_ml_model.py

--- Option_Intraday_ml_model.py
+++ Option_Intraday_ml_model.py
@@ -5,11 +5,6 @@ from scipy.stats import norm
 import yfinance as yf
 import pandas as pd
 
-from datetime import datetime
-from zoneinfo import ZoneInfo
-
-now_ist = datetime.now(ZoneInfo("Asia/Kolkata"))
-
 # --- Parameters ---
 RISK_FREE_RATE = 0.068  # 6.8%
 IV_THRESHOLD = 25       # IV Crush Warning
@@ -66,9 +61,9 @@ def black_scholes_price(S, K, T, r, sigma, option_type="call"):
 
 def get_best_option(symbol):
     now = now_ist.time()
-    if now < TRADING_START or now > TRADING_END:
-        print(f"\n⏳ Outside trading window ({TRADING_START} - {TRADING_END}). Skipping {symbol}.")
-        return
+    # if now < TRADING_START or now > TRADING_END:
+    #     print(f"\n⏳ Outside trading window ({TRADING_START} - {TRADING_END}). Skipping {symbol}.")
+    #     return
 
     print(f"\n📊 Checking {symbol}...")
     try:
